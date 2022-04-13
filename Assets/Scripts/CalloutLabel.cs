830720d574732f3c51ad8c500d62db4a7f6a7892
JokingJester <81639209+JokingJester@users.noreply.github.com>
2022-04-13 13:45:29 -0500

    Muscle Callout Label Works

---
+++ Assets/Scripts/CalloutLabel.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalloutLabel : MonoBehaviour
+{
+    public LineRenderer line;
+    public GameObject targetObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        line.startWidth = 0.01f;
+        line.endWidth = 0.01f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        line.enabled = UIManager.Instance.currentAnim == 1;
+        line.SetPosition(0, this.transform.position);
+        line.SetPosition(1, targetObj.transform.position);
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,8 +5,25 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    private static UIManager _instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                Debug.LogError("UI Manager is null");
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
     public Animator anim;
     public int currentAnim;
+    public GameObject label;
     public void Next()
     {
         //if current anim == 2 return
@@ -16,6 +33,21 @@ public class UIManager : MonoBehaviour
         anim.SetInteger("NextAnim", currentAnim);
     }
 
+    public void ToggleLabel()
+    {
+        if (currentAnim == 1)
+        {
+            label.SetActive(true);
+        }
+        else if(currentAnim != 1)
+        {
+            label.SetActive(false);
+        }
+    }
+    private void Update()
+    {
+        ToggleLabel();
+    }
     public void Previous()
     {
         if (currentAnim == 0)
